da2086c94a6ce33c5e2fb4344714afa43714c5c6
Scapogo <scapogo@gmail.comgit config --global user.email scapogo@gmail.com>
2016-03-01 19:11:29 +0000

    Added more code to basic file

--- RobotPi.py
+++ RobotPi.py
@@ -4,11 +4,22 @@ print("Welcome to robot interface.\n")
 
 direction = 99
 
-while direction != 0:
+#accept user input for robot control
+while True:	
 	direction = int(input("Select robot direction:"))
 	if direction == 2:
 		print("Driving backward.\n")
 	elif direction == 4:
 		print("Driving left.\n")
+	elif direction == 5:
+		print('Robot stopped.\n')
+	elif direction == 6:
+		print("Driving right.\n")
+	elif direction == 8:
+		print('Driving forward.\n')
 	elif direction == 0:
-		exit()
+		break
+	else:
+		print('Not valid command.\n')
+
+print('Finishing program')
