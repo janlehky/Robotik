6c2fc4f638fdd0e6dc1129be0fa99ed67c159964
Scapogo <scapogo@gmail.comgit config --global user.email scapogo@gmail.com>
2016-03-01 19:38:39 +0000

    Finaly controling pololu servo drive

--- RobotPi.py
+++ RobotPi.py
@@ -1,8 +1,12 @@
 #Main robot code
+from Pololu import ServoController
 
 print("Welcome to robot interface.\n")
 
 direction = 99
+
+#create instance of servo class and open channel
+servo_drive = ServoController()
 
 #accept user input for robot control
 while True:	
@@ -13,13 +17,20 @@ while True:
 		print("Driving left.\n")
 	elif direction == 5:
 		print('Robot stopped.\n')
+		servo_drive.setPosition(1,1500)
+		servo_drive.setPosition(2,1500)
 	elif direction == 6:
 		print("Driving right.\n")
 	elif direction == 8:
 		print('Driving forward.\n')
+		servo_drive.setPosition(1,500)
+		servo_drive.setPosition(2,500)
 	elif direction == 0:
 		break
 	else:
 		print('Not valid command.\n')
 
 print('Finishing program')
+
+#close servo controler
+servo_drive.closeServo()
