5125451215d558572d489cf035c3382829f6ea27
Scapogo <scapogo@gmail.comgit config --global user.email scapogo@gmail.com>
2016-03-06 18:28:24 +0000

    modified class after testing on live system

--- RobotPi.py
+++ RobotPi.py
@@ -1,13 +1,9 @@
 #Main robot code
-#from Pololu import ServoController
 from robot import Robot
 
 print("Welcome to robot interface.\n")
 
 direction = 99
-
-#create instance of servo class and open channel
-#servo_drive = ServoController()
 
 #create robot class intance
 robot = Robot()
@@ -24,23 +20,18 @@ while True:
 	elif direction == 5:
 		print('Robot stopped.\n')
 		robot.stop_robot()
-		#servo_drive.setPosition(1,1500)
-		#servo_drive.setPosition(2,1500)
 	elif direction == 6:
 		print("Driving right.\n")
 		robot.turn_right()
 	elif direction == 8:
 		print('Driving forward.\n')
 		robot.go_forward()
-		#servo_drive.setPosition(1,500)
-		#servo_drive.setPosition(2,500)
 	elif direction == 0:
 		break
 	else:
 		print('Not valid command.\n')
 
-print('Finishing program')
+print('Finishing program.')
 
 #close servo controler
-#servo_drive.closeServo()
 robot.close_servo_interface()
