4dfc7f3245373f8301d4cf7d3e82f2d30ed578f7
Scapogo <scapogo@gmail.com>
2016-03-17 23:33:30 +0100

    Update RobotServer.py
    Changed implementation to threading clas

--- RobotServer.py
+++ RobotServer.py
@@ -1,53 +1,57 @@
 #code for server side of robot
 import socket
 import sys
-from thread import *
+import threading
  
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
+print('Socket created')
  
 #Bind socket to local host and port
 try:
     s.bind((HOST, PORT))
 except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
      
-print 'Socket bind complete'
+print('Socket bind complete')
  
 #Start listening on socket
 s.listen(10)
-print 'Socket now listening'
- 
-#Function for handling connections. This will be used to create threads
-def clientthread(conn):
-    #Sending message to connected client
-    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+print('Socket now listening')
+
+class clientThread(threading.Thread):
+    """Thread to accept commands from clients"""
+    def __init__(self, connection)
+        threading.Thread.__init__(self)
+        self.connection = connection
+    
+    def run(self):
+        conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
      
-    #infinite loop so that function do not terminate and thread do not end.
-    while True:
+        #infinite loop so that function do not terminate and thread do not end.
+        while True:
          
-        #Receiving from client
-        data = conn.recv(1024)
-        reply = 'OK...' + data
-        if not data: 
-            break
+            #Receiving from client
+            data = conn.recv(1024)
+            reply = 'OK...' + data
+            if not data: 
+                break
      
-        conn.sendall(reply)
+            conn.sendall(reply)
      
-    #came out of loop
-    conn.close()
- 
+        #came out of loop
+        conn.close()
+
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    print('Connected with ' + addr[0] + ':' + str(addr[1]))
      
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,))
+    connection_thread = clientThread(conn)
  
 s.close()
