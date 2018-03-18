0356c4013bcd9249cfd9ffced1c2205239a44d13
scapogo <scapogo@gmail.com>
2018-03-18 10:52:35 +0100

    added exceptions

--- Multiprocess_Robot.py
+++ Multiprocess_Robot.py
@@ -11,22 +11,30 @@ x = multiprocessing.Value('d', 100.0)
 p = multiprocessing.Process(target=d.measure, args=(x, ))
 p.start()
 
-for i in range(10):
+try:
+    for i in range(10):
 
-    if not p.is_alive():
-        p = multiprocessing.Process(target=d.measure, args=(x, ))
-        p.start()
+        if not p.is_alive():
+            p = multiprocessing.Process(target=d.measure, args=(x, ))
+            p.start()
 
-    print(x.value)
-    if x.value >= 50:
-        robotik.go_forward()
-    elif x.value < 10:
-        robotik.go_backward()
-    elif (x.value >= 10) and (x.value < 50):
-        robotik.turn_left()
+        print(x.value)
+        if x.value >= 50:
+            robotik.go_forward()
+        elif x.value < 10:
+            robotik.go_backward()
+        elif (x.value >= 10) and (x.value < 50):
+            robotik.turn_left()
 
-    time.sleep(0.3)
+        time.sleep(0.3)
 
-p.terminate()
-d.cleanup()
+except KeyboardInterrupt:
+    print('Finishing')
+
+except:
+    print('Other error')
+
+finally:
+    p.terminate()
+    d.cleanup()
 
