f809052e52638a614a4771db6f2d64cde35055fa
Jan Lehky <scapogo@gmail.com>
2018-05-09 18:17:44 +0000

    fixed car control to stop after ctrl+c and speed tune

--- Car_Control.py
+++ Car_Control.py
@@ -51,50 +51,61 @@ def drive_vehicle(x_offset, y_offset, run_cmd, width, front_distance_sensor):
     right_bwd = False
 
     while True:
-        if front_distance_sensor.value < 5 or width.value > 200:
-            left_speed = 0
-            right_speed = 0
-            left_fwd = left_bwd = right_fwd = right_bwd = False
-        else:
-            right_fwd = True
-            if x_offset.value == -10:
-                left_speed = 0.6 * max_speed
-                left_fwd = False
-                left_bwd = True
-                right_speed = 0.6 * max_speed
-            elif -5 < x_offset.value < 0:
-                left_speed = abs(x_offset.value) * max_speed
-                right_speed = max_speed
-                left_fwd = True
-                left_bwd = False
-            elif x_offset.value > 0:
-                left_speed = max_speed
-                right_speed = x_offset.value * max_speed
-                left_fwd = True
-                left_bwd = False
+        try:
+            if front_distance_sensor.value < 5 or width.value > 500:
+                left_speed = 0
+                right_speed = 0
+                left_fwd = left_bwd = right_fwd = right_bwd = False
             else:
-                left_speed = max_speed
-                right_speed = max_speed
-                left_fwd = True
-                left_bwd = False
+                right_fwd = True
+                if x_offset.value == -10:
+                    left_speed = 0.6 * max_speed
+                    left_fwd = False
+                    left_bwd = True
+                    right_speed = 0.8 * max_speed
+                elif -5 < x_offset.value < 0:
+                    left_speed = abs(x_offset.value) * max_speed
+                    right_speed = max_speed
+                    left_fwd = True
+                    left_bwd = False
+                elif x_offset.value > 0:
+                    left_speed = max_speed
+                    right_speed = x_offset.value * max_speed
+                    left_fwd = True
+                    left_bwd = False
+                else:
+                    left_speed = max_speed
+                    right_speed = max_speed
+                    left_fwd = True
+                    left_bwd = False
         
-        print('Speeds: Left {} Right {}'.format(left_speed, right_speed))
+            print('Speeds: Left {} Right {} Run {}'.format(left_speed, right_speed, run_cmd.value))
 
-        # Right drives
-        pwm.set_pwm(0, 0, int(right_speed*run_cmd.value))
-        pwm.set_pwm(1, 0, int(right_speed*run_cmd.value))
-        GPIO.output(left_fwd_pin_1, left_fwd)
-        GPIO.output(left_fwd_pin_2, left_fwd)
-        GPIO.output(left_bwd_pin_1, left_bwd)
-        GPIO.output(left_bwd_pin_2, left_bwd)
+            # Right drives
+            pwm.set_pwm(0, 0, int(right_speed*run_cmd.value))
+            pwm.set_pwm(1, 0, int(right_speed*run_cmd.value))
+            GPIO.output(left_fwd_pin_1, left_fwd)
+            GPIO.output(left_fwd_pin_2, left_fwd)
+            GPIO.output(left_bwd_pin_1, left_bwd)
+            GPIO.output(left_bwd_pin_2, left_bwd)
 
-        # Left drives
-        pwm.set_pwm(4, 0, int(left_speed*run_cmd.value))
-        pwm.set_pwm(5, 0, int(left_speed*run_cmd.value))
-        GPIO.output(right_fwd_pin_1, right_fwd)
-        GPIO.output(right_fwd_pin_2, right_fwd)
-        GPIO.output(right_bwd_pin_1, right_bwd)
-        GPIO.output(right_bwd_pin_2, right_bwd)
+            # Left drives
+            pwm.set_pwm(4, 0, int(left_speed*run_cmd.value))
+            pwm.set_pwm(5, 0, int(left_speed*run_cmd.value))
+            GPIO.output(right_fwd_pin_1, right_fwd)
+            GPIO.output(right_fwd_pin_2, right_fwd)
+            GPIO.output(right_bwd_pin_1, right_bwd)
+            GPIO.output(right_bwd_pin_2, right_bwd)
+        except KeyboardInterrupt:
+            GPIO.output(left_fwd_pin_1, False)
+            GPIO.output(left_fwd_pin_2, False)
+            GPIO.output(left_bwd_pin_1, False)
+            GPIO.output(left_bwd_pin_2, False)
+            GPIO.output(right_fwd_pin_1, False)
+            GPIO.output(right_fwd_pin_2, False)
+            GPIO.output(right_bwd_pin_1, False)
+            GPIO.output(right_bwd_pin_2, False)
+            GPIO.cleanup()
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -119,7 +130,7 @@ def on_message(client, userdata, msg):
                 x_offset.value = float(parts[1])
             elif 'width' in parts[0]:
                 print('Width: {}'.format(parts[1]))
-                width.value = float(parts[1])
+                width.value = float(parts[1][:-1])
             elif 'y' in parts[0]:
                 print('Y offset: {}'.format(parts[1]))
                 y_offset.value = float(parts[1])
@@ -134,27 +145,24 @@ rabitmq_ip = '192.168.2.88'
 client.username_pw_set('client', '1234')
 client.connect(rabitmq_ip, 1883, 60)
 
-# Create distance measurement process and start it
-drive_process = multiprocessing.Process(target=drive_vehicle,
-                                        args=(x_offset, y_offset, run_cmd, width, front_distance_sensor, ))
-drive_process.start()
-
-# Create measurement object for front sensor
-#front_measurement = Distance(20, 21)
-# Create front measurement process and start it
-#front_measurement_process = multiprocessing.Process(target=front_measurement.measure, args=(front_distance_sensor, ))
-#front_measurement_process.start()
-
 try:
+    # Create distance measurement process and start it
+    drive_process = multiprocessing.Process(target=drive_vehicle,
+                                        args=(x_offset, y_offset, run_cmd, width, front_distance_sensor, ))
+    drive_process.start()
+
+    # Create measurement object for front sensor
+    #front_measurement = Distance(20, 21)
+    # Create front measurement process and start it
+    #front_measurement_process = multiprocessing.Process(target=front_measurement.measure, args=(front_distance_sensor, ))
+    #front_measurement_process.start()
+
     client.loop_forever()
 finally:
     # disconnect from rabitmq
+    print("Disconnect")
     client.disconnect()
-    # set stop command
-    run_cmd.value = 0.0
-    time.sleep(2)
     # Terminate running processes and cleanup claimed GPIO's
     drive_process.terminate()
     # front_measurement_process.terminate()
     # front_measurement.cleanup()
-    GPIO.cleanup()
